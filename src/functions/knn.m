e3e508495859e4468abc3e1bb9847ea6b783ae0b
Florentin Hennecker <florentin.hennecker@gmail.com>
2015-12-04 14:37:08 +0000

    [add] KNN function

---
+++ src/functions/knn.m
@@ -0,0 +1,16 @@
+function [ class ] = knn( X, T, input, k)
+%knn compute class of input with KNN on feature vectors X with targets T
+    closest = [Inf(k, 1) zeros(k, 1)]; % distance-class vector
+    for x=1:size(X,1)
+        i=1;
+        % sorted insert if we find a closer point
+        while (i <= size(closest, 1) && norm(X(x)-input) < closest(i))
+            if (i ~= 1)
+                closest(i-1, :) = closest(i, :);
+            end
+            closest(i, :) = [norm(X(x)-input) T(x)];
+            i = i+1;
+        end
+    end
+end
+
